d5303c4fc4837949c73cd7e541581f2d6657b1ec
Andronicus Lepcha <androlepcha843@gmail.com>
2022-11-18 21:30:38 +0530

    weather app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weather-app';
+  title = 'project-1';
+  count=1
+  place=["gantok","siliguri","Namchi"]
+  A=0;
+  B=0;
+  C=0;
+  tiles: any = [
+    {text: {location:"gangtok",temp:"16",humidity:"78",sunrise:"6:45",sunset:"4:48"}, cols: 3, rows: 1, color: 'lightblue'},
+    {text: {location:"gangtok",temp:"16",humidity:"78",sunrise:"6:45",sunset:"4:48"}, cols: 1, rows: 2, color: 'lightgreen'},
+    {text: {location:"gangtok",temp:"16",humidity:"78",sunrise:"6:45",sunset:"4:48"}, cols: 1, rows: 1, color: 'lightpink'},
+    {text: {location:"gangtok",temp:"16",humidity:"78",sunrise:"6:45",sunset:"4:48"}, cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+  add=()=>{
+    this.C=this.A+this.B
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+//Please go and search the angular materail and do the need full
 
 @NgModule({
   declarations: [
@@ -9,6 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    ,FormsModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
